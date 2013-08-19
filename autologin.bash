2e782f4e5bb9ba539e33a66fc8424932990b4f22
monorailpilot <monorailpilot@gmail.com>
2013-08-19 15:19:50 -0400

    Initial Commit

---
+++ autologin.bash
@@ -0,0 +1,14 @@
+#Place in your bashrc/bashprofile to automatically ssh to remote hosts (IE just type the host name at a bash prompt to initiate ssh).
+
+###Auto Login coolness
+command_not_found_handle () {
+    if [[ ! "$1" ]] ; then
+        return 127
+    fi
+
+    ssh $1 2>/dev/null
+    if [ $? -eq 255 ] ; then
+        ERROR=$?
+        echo "-bash: $1: command not found"
+        return $ERROR;
+    fi
