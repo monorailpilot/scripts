f1a52960c1ce5eac9f8168284257fdd660db18f3
monorailpilot <monorailpilot@gmail.com>
2013-08-28 14:57:31 -0400

    Check for not found results.

--- dns_check.pl
+++ dns_check.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 #
+#Quick and dirty DNS check, display average response times, not founds and other goodies
 if ($#ARGV!=3) {
 	print "\nUsage: dns_check.pl <HOST> <DNS SERVER> <REPETITIONS> <DISPLAY THRESHOLD IN MS>\n\n";
 	exit(1);
@@ -10,15 +11,22 @@ $times=$ARGV[2];
 $threshold=$ARGV[3];
 $total=0;
 $count=0;
+$notfound=0;
 $max=0;
 
 while ($count < $times) {
 	@command=`dig $host \@$server`;
 	@ms= grep { /Query time: \d+ msec/ } @command;
+	@result= grep { /ANSWER SECTION/ } @command;
 	$ms[0] =~/(\d+)/;	
-	if ($1 > $threshold) {
+	if (($1 > $threshold) || (!@result)){
 		print "Attempt:";
-		print $count+1 . " " . $1 . "ms\n";
+		print $count+1 . " " . $1 . "ms";
+		if (!@result) {
+			print " Not Found";
+			$notfound++;
+		}
+		print "\n";
 	};
 	$total+=$1;
 	if ($max < $1) {
@@ -26,5 +34,5 @@ while ($count < $times) {
 	}
 	$count++;
 }
-print "Max: $max ms, Avg: ".$total/$count."ms\n";
+print "Max: $max ms, Avg: ".$total/$count."ms Not Found:$notfound\n";
 
